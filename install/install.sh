e9b4d5c47c5f41c6f0ab61d07c773d496a34f120
root <ricardojruiz93@gmail.com>
2019-10-10 03:49:13 -0400

    Deleted repos

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/bitquark-subdomains-top100000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/bitquark-subdomains-top100000.txt -O ../APIs/bitquark-subdomains-top100000.txt
 pip install dnsdumpster
-git clone https://github.com/PaulSec/API-dnsdumpster.com APIs/API-dnsdumpster.com
-cp APIs/dnsdumpster_api.py APIs/API-dnsdumpster.com/api.py
+git clone https://github.com/PaulSec/API-dnsdumpster.com ../APIs/API-dnsdumpster.com
+cp ../APIs/dnsdumpster_api.py ../APIs/API-dnsdumpster.com/api.py
